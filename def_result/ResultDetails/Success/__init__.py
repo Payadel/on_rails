0215149daad051e4459b4038b1e708e634e94275
Hamid Molareza <kitm.hamid@gmail.com>
2023-04-02 19:23:30 +0330

    fix!: rename `def_result` to `on_rails`

--- def_result/ResultDetails/Success/__init__.py
+++
@@ -1,3 +0,0 @@
-from def_result.ResultDetails.Success.CreatedDetail import *
-from def_result.ResultDetails.Success.NotModifiedDetail import *
-from def_result.ResultDetails.Success.PartialContentDetail import *
--- def_result/__init__.py
+++
@@ -1,3 +0,0 @@
-from def_result.decorator import *
-from def_result.Result import *
-from def_result.ResultDetail import *
