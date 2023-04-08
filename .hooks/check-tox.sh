80e6161330b37be42a8f522f0e603cd671e2a688
Hamid Molareza <kitm.hamid@gmail.com>
2023-04-08 10:53:02 +0330

    ci: update .pre-commit-config.yaml and git hook script

---
+++ .hooks/check-tox.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [ $# == 0 ]; then
+    echo "No branch name is given, so this script will be executed on all branches."
+    tox
+    exit 0
+fi
+
+# Get the current branch name
+current_branch=$(git symbolic-ref --short HEAD)
+
+# Loop through the input parameters (i.e., the list of branch names)
+for branch in "$@"; do
+    # Check if the current branch matches the current input parameter
+    if [ "$current_branch" = "$branch" ]; then
+        # If it does, execute the 'tox' command
+        tox
+        exit 0
+    fi
+done
+
+# If the current branch does not match any of the input parameters, print a message
+echo "Current branch '$current_branch' does not match any of the specified branch names: $*"
+exit 0
