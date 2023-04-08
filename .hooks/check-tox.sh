7b13058e48cdaaa3c9c67bbffda629e877584516
Hamid Molareza <kitm.hamid@gmail.com>
2023-04-08 11:13:15 +0330

    ci: fix check-tox.sh

--- .hooks/check-tox.sh
+++ .hooks/check-tox.sh
@@ -3,7 +3,7 @@
 if [ $# == 0 ]; then
     echo "No branch name is given, so this script will be executed on all branches."
     tox
-    exit 0
+    exit $?
 fi
 
 # Get the current branch name
@@ -15,7 +15,7 @@ for branch in "$@"; do
     if [ "$current_branch" = "$branch" ]; then
         # If it does, execute the 'tox' command
         tox
-        exit 0
+        exit $?
     fi
 done
 
